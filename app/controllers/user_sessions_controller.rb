8cd35c4bd9d012eb450721e1ca92c583c251e1d6
An.nA <anna.git3@gmail.com>
2015-01-30 01:00:08 -0800

    modify controllers

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -4,7 +4,16 @@ class UserSessionsController < ApplicationController
 
   def create
   	user = User.find_by(email: params[:email])
-  	user.authenticate(params[:password])
-  	redirect_to todo_lists_path
+  	if user && user.authenticate(params[:password])
+  		#set session variable user_id to the @user's id
+  		# we store the currently logged in user's id in our rails session
+	  	session[:user_id] = user.id
+	  	flash[:success] = "Thanks for logging in!"
+	  	redirect_to todo_lists_path
+  	else
+  		flash[:error] = "There was a problem logging in. Please check our email and password."
+  		render action: 'new'
+  	end
+
   end
 end
--- spec/controllers/user_sessions_controller_spec.rb
+++ spec/controllers/user_sessions_controller_spec.rb
@@ -37,10 +37,57 @@ RSpec.describe UserSessionsController, :type => :controller do
         #deprecated:
         #User.stub(:find_by).and_return(user)
         allow(User).to receive(:find_by).and_return(user)
+        # expect the found user object to receive the authenticate method
         expect(user).to receive(:authenticate)
         post :create, email: "jabba@thehut.com", password: "jabba12345"
       end
+
+      it "sets the user_id in the session" do
+        #allows rails app to know the user is logged in
+        post :create, email: "jabba@thehut.com", password: "jabba12345"
+        expect(session[:user_id]).to eq(user.id)
+      end
+
+      it "sets the flash success message" do
+        post :create, email: "jabba@thehut.com", password: "jabba12345"
+        expect(flash[:success]).to eq("Thanks for logging in!")
+      end
     end
+
+
+    shared_examples_for "denied login" do
+      it "renders the new template" do
+        post :create, email: email, password: password
+        expect(response).to render_template('new')
+      end
+
+      it "sets the flash error message" do
+        post :create
+        expect(flash[:error]).to eq("There was a problem logging in. Please check our email and password.")
+      end
+    end
+
+
+    context "with blank credentials" do
+      let(:email) { "" }
+      let(:password) { "" }
+      it_behaves_like "denied login"
+    end
+
+    context "with an incorrect password" do
+      let!(:user) { User.create(first_name: "Jabba", last_name: "Thehut", email: "jabba@thehut.com", password: "jabba12345", password_confirmation: "jabba12345") }
+      let(:email) { user.email }
+      let(:password) { "hello" }
+      it_behaves_like "denied login"
+    end
+
+    context "with no email in existence" do
+      let(:email) { "none@found.com" }
+      let(:password) { "password" }
+      it_behaves_like "denied login"
+    end
+
+      
 
   end
 
